109290c8514c92b97eef75d481dff120fe1ed456
Heidi Enbuska <henbuska@student.hive.fi>
2025-02-13 14:37:58 +0200

    Add structs to header file

--- include/miniRT.h
+++ include/miniRT.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: henbuska <henbuska@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:10:16 by irychkov          #+#    #+#             */
-/*   Updated: 2025/02/13 14:10:59 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/02/13 14:34:39 by henbuska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,146 @@
 
 #define WIDTH 512
 #define HEIGHT 512
+
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdint.h>
+
+//# include "MLX42.h"
+//# include "libft.h"
+
+#define M_PI 3.14159265358979323846  // how accurate should this be?
+#define EPSILON 0.00001
+#define WIDTH 512
+#define HEIGHT 512
+
+typedef struct	s_rt
+{
+	//mlx_t	*mlx;
+	//t_image	*image;
+	//t_scene		*scene;
+	char		*filename;
+	
+}	t_rt;
+
+typedef struct	s_tuple
+{
+	double	x;
+	double	y;
+	double	z;
+	double	w;
+}	t_tuple;
+
+typedef struct	s_matrix
+{
+	int		size;
+	double	data[4][4];
+}	t_matrix;
+
+typedef struct	s_ray
+{
+	t_tuple	origin;
+	t_tuple	direction;
+}	t_ray;
+
+typedef struct	s_material
+{
+	t_tuple		color;
+	//double	ambient;
+	double		diffuse;
+	double		specular;
+	double		shininess;
+	//t_pattern	pattern;
+}	t_material;
+
+typedef enum e_type
+{
+	SPHERE,
+	PLANE,
+	//CUBE,
+	CYLINDER,
+	CONE,
+}	t_type;
+
+typedef struct	s_shape
+{
+	t_matrix	transform;
+	t_material	material;
+	t_type		type;
+	t_tuple		center;
+	double		radius;
+	double		min;
+	double		max;
+	bool		closed;
+}	t_shape;
+
+typedef struct	s_camera
+{
+	double		hsize;
+	double		vsize;
+	double		field_of_view;
+	double		half_width;
+	double		half_height;
+	t_matrix	transform;
+}	t_camera;
+
+typedef struct	s_light
+{
+	t_tuple	position;
+	t_tuple	intensity;
+}	t_light;
+
+typedef struct	s_ambient
+{
+	double	ratio;
+	t_tuple	color;
+}	t_ambient;
+
+typedef struct	s_scene
+{
+	t_shape		*shapes;
+	t_camera	*camera;
+	t_light		*light;
+	t_ambient	ambient;
+
+	int			num_objects;
+	int			max_objects;;
+}	t_scene;
+
+typedef struct	s_comps
+{
+	double		t;
+	t_shape		object;
+	t_tuple		point;
+	t_tuple		eyev;
+	t_tuple		normalv;
+	bool		inside;
+	t_tuple		over_point;
+}	t_comps;
+
+typedef struct	s_intersection
+{
+	double		t;
+	t_shape		object;
+}	t_intersection;
+
+typedef struct	s_intersections
+{
+	int				count;
+	t_intersection	*list;
+}	t_intersections;
+
+
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_calloc(size_t count, size_t size);
+
+#endif
 
 #endif
