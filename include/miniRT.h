9e2133da9d4c31e238c6fb0a47643a6b0b45503c
Rychkov Iurii <irychkov@student.hive.fi>
2025-02-13 14:15:46 +0200

    Added Makefile. Connected to libft. Installed MLX. Tested.

---
+++ include/miniRT.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 14:10:16 by irychkov          #+#    #+#             */
+/*   Updated: 2025/02/13 14:10:59 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <MLX42/MLX42.h>
+
+#define WIDTH 512
+#define HEIGHT 512
+
+#endif
